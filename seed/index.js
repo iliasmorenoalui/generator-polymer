5276bdd049130eaa1220a58ff3e9e65b8069a66a
Ilias Moreno <contacto@iliasmoreno.com>
2015-07-09 13:43:23 +0200

    revert change

--- seed/index.js
+++ seed/index.js
@@ -17,7 +17,7 @@ module.exports = yeoman.generators.Base.extend({
 
     this.option('skip-install', {
       desc: 'Whether bower dependencies should be installed',
-      defaults: true,
+      defaults: false,
     });
 
     this.option('skip-install-message', {
@@ -99,7 +99,6 @@ module.exports = yeoman.generators.Base.extend({
     this.copy('index.html', 'index.html', renameElement);
     this.copy('README.md', 'README.md', renameElement);
     this.copy('seed-element.html', this.elementName + '.html', renameElement);
-    this.copy('seed-element.scss', this.elementName + '.scss', renameElement);
     this.copy('demo/index.html', 'demo/index.html', renameElement);
 
     if (this.includeWCT) {
